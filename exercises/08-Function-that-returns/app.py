8fcc9d855f3b4e543c40c07c54e4f2bc1fe2e3ab
Pilarzmt <140454440+Pilarzmt@users.noreply.github.com>
2023-08-12 18:41:02 +0000

    Pending changes exported from your codespace

--- exercises/08-Function-that-returns/app.py
+++ exercises/08-Function-that-returns/app.py
@@ -5,3 +5,9 @@ def euro_to_yen(euro_value):
 	return euro_value * 124.15
 
 ####### ↓ YOUR CODE BELOW ↓ #######
+
+dollar = 137
+euro_amount = dollar * 0.89
+
+yen_amount = euro_amount * 124.15
+print(yen_amount)
--- exercises/09-Function-parameters/app.py
+++ exercises/09-Function-parameters/app.py
@@ -2,6 +2,15 @@
 def render_person(param):
     return param
 
+def render_person(name, age, gender, birthdate, eye_color):
+    person_info = f"{name} is a {age} years old {gender} born in {birthdate} with {eye_color} eyes"
+    return person_info
+
+green = eye_color
+5/22/1983 = birthday
+Bob = name
+23 = age
+male = gender
 
 # Do not edit below this line
 print(render_person('Bob', '05/22/1983', 'green', 23, 'male'))
